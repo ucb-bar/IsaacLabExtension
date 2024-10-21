51205eaadc8ad0c9eb9416bbf4d9c1906b188073
-T.K.- <t_k_233@outlook.com>
2024-10-20 19:44:30 -0700

    ADD: rename project

--- scripts/rsl_rl/play.py
+++ scripts/rsl_rl/play.py
@@ -50,7 +50,7 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
 )
 
 # Import extensions to set up environment tasks
-import ext_template.tasks  # noqa: F401
+import g1_template.tasks  # noqa: F401
 
 
 def main():
